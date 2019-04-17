0c831fcc0b1c80f00134837abb7c8a36f74f0072
mbjorck <35528768+mbjorck@users.noreply.github.com>
2019-04-17 14:57:55 +0200

    Uploading old project

---
+++ WeatherApi/Models/RootObject.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApi.Models
+{
+    public class RootObject
+    {
+        [JsonProperty("station")]
+        public Station[] Stations { get; set; }
+        //public Station Stations { get; set; }
+        ////[JsonProperty("updated")]
+        //public Int64 updated { get; set; }
+
+    }
+
+    public class Station
+    {
+        //private string key;
+        [JsonProperty("key")]
+        public string Key { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("owner")]
+        public string Owner { get; set; }
+        [JsonProperty("from")]
+        public Int64 From { get; set; }
+        [JsonProperty("to")]
+        public Int64 To { get; set; }
+        [JsonProperty("latitude")]
+        public float Latitude { get; set; }
+        [JsonProperty("longitude")]
+        public float Longitude { get; set; }
+        [JsonProperty("value")]
+        public Value[] Values { get; set; }
+        //public List<Value> Values { get; set; }
+        //public string Key { get => key; set => key = value; }
+    }
+    public class Value
+    {
+        [JsonProperty("date")]
+        public Int64 Date { get; set; }
+        [JsonProperty("value")]
+        public float value { get; set; }
+        [JsonProperty("ref")]
+        public string Ref { get; set; }
+        [JsonProperty("quality")]
+        public string Quality { get; set; }
+
+    }
+           
+}
---
+++ WeatherApi/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+using System.Collections.Generic;
+using WeatherApi.Models;
+using System.Net.Http.Headers;
+using System.Threading;
+
+namespace WeatherApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //GetAverageTempForLastHour().Wait();
+            //GetTotalRainfallInLund().Wait();
+            PrintStations().Wait();
+
+        }
+
+        static async Task GetAverageTempForLastHour()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://opendata-download-metobs.smhi.se/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = await client.GetAsync("api/version/1.0/parameter/1/station-set/all/period/latest-hour/data.json");
+
+                if (response.IsSuccessStatusCode)
+                {
+
+                    var jsonString = await response.Content.ReadAsStringAsync();
+                    var _Data = JsonConvert.DeserializeObject<RootObject>(jsonString);
+
+                    float totalTemp = 0;
+                    int numberOfValues = 0;
+
+                    foreach (Station station in _Data.Stations)
+                    {
+
+
+                        if (station.Values != null)
+                        {
+                            foreach (Value values in station.Values)
+                            {
+                                totalTemp = totalTemp + values.value;
+                                numberOfValues++;
+                            }
+                        }
+                    }
+                    float averageTemp = totalTemp / numberOfValues;
+                    Console.WriteLine("The average temperature in Sweden for the last hour was " + averageTemp + " degrees");
+                    Console.ReadKey();
+                }
+            }
+        }
+
+        static async Task GetTotalRainfallInLund()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://opendata-download-metobs.smhi.se/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = await client.GetAsync("api/version/1.0/parameter/5/station/53430/period/latest-months/data.json");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonString = await response.Content.ReadAsStringAsync();
+                    var _Data = JsonConvert.DeserializeObject<Station>(jsonString);
+                    float totalRainAmount = 0;
+                    List<string> dates = new List<string>();
+                    int numberOfIndices = 0;
+                    foreach (Value values in _Data.Values)
+                    {
+                        totalRainAmount += values.value;
+                        dates.Add(values.Ref);
+                        numberOfIndices++;
+
+                    }
+                    Console.WriteLine("Between " + dates[0] + " and " + dates[(numberOfIndices - 1)] + " the total rainfall in Lund was " + totalRainAmount + " millimeters");
+                    Console.ReadKey();
+                }
+            }
+        }
+
+        static async Task PrintStations()
+        {
+           
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://opendata-download-metobs.smhi.se/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = await client.GetAsync("api/version/1.0/parameter/1/station-set/all/period/latest-hour/data.json");
+
+                if (response.IsSuccessStatusCode)
+                {
+
+                    var jsonString = await response.Content.ReadAsStringAsync();
+                    var _Data = JsonConvert.DeserializeObject<RootObject>(jsonString);
+                    CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+                    CancellationToken token = cancellationTokenSource.Token;
+                    Task task = Task.Run(() =>
+                    {
+                        while (!token.IsCancellationRequested)
+                        {
+                            foreach (Station station in _Data.Stations)
+                            {
+
+                                Console.Write(station.Name + ": ");
+                                if (station.Values != null)
+                                {
+                                    foreach (Value values in station.Values)
+                                    {
+                                        Console.WriteLine(values.value);
+                                        Thread.Sleep(100);
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("");
+                                    Thread.Sleep(100);
+                                }
+                            }
+                          
+                        }
+                        token.ThrowIfCancellationRequested();
+                    }, token)
+                    .ContinueWith(t =>
+                    {
+                        t.Exception?.Handle(e => true);
+                        Console.WriteLine("You have canceled the task");
+                    }, TaskContinuationOptions.OnlyOnCanceled);
+
+                    Console.WriteLine("Press enter to stop the task");
+                    Console.ReadLine();
+                    cancellationTokenSource.Cancel();
+                    task.Wait();
+
+                    
+                } else
+                {
+                    throw new Exception("ERROR");
+                }
+            }
+        }
+    }
+}
